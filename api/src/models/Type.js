677a334abcab8264cc6da3c5540cf9d3d550410a
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-02-01 16:47:12 -0300

    Pokemon Types DB model Created

---
+++ api/src/models/Type.js
@@ -0,0 +1,19 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('type', {
+    ID: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+  
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      }
+    });
+};
