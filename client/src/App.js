60b7247b48a9e736ca928bd7db2699c82549bbff
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-02-08 12:30:41 -0300

    App updated with BrowserRouter, Rotes & Route, added Landing Page & Home Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
+import LandingPage from './Components/React/LandingPage';
+import Home from './Components/React/Home';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <h1>Henry Pokemon</h1>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path = '/' element= {<LandingPage/>} />
+          <Route path = '/home' element= {<Home/>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
