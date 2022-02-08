41b90325ef7893e61afc024b2c2f572e58eb5ded
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-02-08 12:34:46 -0300

    Reducer initial State updated

--- client/src/Reducers/index.js
+++ client/src/Reducers/index.js
@@ -1,5 +1,29 @@
-function rootReducer() {
 
+const initialState = {
+    pokemon: {},
+    pokemons: [],
+    pokemonsFiltered: [],
+    pokemonsTypes: []
+}
+
+
+function rootReducer(state = initialState, action) {
+    switch(action.type) {
+        case 'GET_POKEMONS':
+            return {
+                ...state,
+                pokemons: action.payload
+            }
+
+            case 'GET_ONE_POKE':
+                return {
+                    ...state,
+                    pokemon: action.payload
+                }
+
+     default:
+        return state
+    }
 }
 
 export default rootReducer
