c9a37827e7345019894538b2a9350818f09d70c8
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-02-09 23:39:39 -0300

    Home updated, not finished

--- client/src/Components/React/Home.jsx
+++ client/src/Components/React/Home.jsx
@@ -3,15 +3,29 @@ import {useDispatch, useSelector} from 'react-redux';
 import { getPokes } from '../../Actions';
 import {Link} from 'react-router-dom';
 import Card from './Card'
+import Paginado from './Paginado';
 import '../Styles/Home.css'
 
 export default function Home(){
     const dispatch = useDispatch();
     const allPokemons = useSelector((state) => state.pokemons)
+    // Local States \\
+    const [current, setCurrent] = useState(1)
+    const [pokemonsPage, setPokemonsPage] = useState(12)
+    const lastPoke = current * pokemonsPage
+    const firstPoke = lastPoke - pokemonsPage
+    const actualPokemons = allPokemons.slice(firstPoke, lastPoke)
+    // Paginado \\
+    const paginado = (pageNumber) => {
+        setCurrent(pageNumber)
+    }
+
+
+
 
     useEffect (()=> {
         dispatch(getPokes())
-    },[])
+    },[dispatch])
 
     function handleClick(e){
         e.preventDefault();
@@ -20,27 +34,43 @@ export default function Home(){
 
     return (
         <div className='mainContainer'>
-            <Link to = '/pokemons'>Crear Poke</Link>
-            <h1>Pokemon App</h1>
-            <button onClick={e => {handleClick(e)}}>Recargar Pokemons</button>
-            <div>
-                <select>
-                    <option value="asc">Ascendente</option>
-                    <option value="desc">Descendente</option>
-                </select>
-                <select>
-                    <option value="Todos">Filtrar...</option>
-                    <option value="Tipos">Tipes</option>
-                </select>
-                <select>
-                    <option value="Todos">Filtrar...</option>
-                    <option value="Api">Existentes</option>
-                    <option value="Created">Creados</option>
-                </select>
+            <div className='sb'>
+                <div className='Row'>
+                    <h4 className='text'>Create:
+                    <Link to = '/pokemons'>
+                        <button className='create' />
+                    </Link>
+                    </h4>
+                    <h4 className='text'>Reload: 
+                    <button className='reload' onClick={e => {handleClick(e)}}/>
+                    </h4>
+                    <div className='filters'>
+                        <select className='filters'>
+                            <option value="asc">Ascendente</option>
+                            <option value="desc">Descendente</option>
+                        </select>
+                        <select className='filters'>
+                            <option value="Todos">Filtrar...</option>
+                            <option value="Tipos">Tipes</option>
+                        </select>
+                        <select className='filters'>
+                            <option value="Todos">Filtrar...</option>
+                            <option value="Api">Existentes</option>
+                            <option value="Created">Creados</option>
+                        </select>
+                    </div>
+                </div>
+                <div className='T-P-SB'>
+                    {/*<SearchBar />*/}
+                    <h1 className='Title'>Pokemon App</h1>
+                    <Paginado className="paginado" pokemonsPage = {pokemonsPage} allPokemons = {allPokemons.length} paginado = {paginado} />
+                </div>
+            </div>
+            <div className='Cards'>
                 {
-                    allPokemons && allPokemons.map( p => {
+                    actualPokemons?.map( p => {
                         return(
-                        <Card name={p.name} image={p.image} type={p.type} />
+                        <Card name={p.name} img={p.img} types={p.types} />
                     )})
                 }
             </div>
