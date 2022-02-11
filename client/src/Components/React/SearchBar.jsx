74e07d163df89aded1cbde166bc73d20f4f486c5
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-02-10 21:07:41 -0300

    SearchBar updated to show message so can be organized on Home

--- client/src/Components/React/SearchBar.jsx
+++ client/src/Components/React/SearchBar.jsx
@@ -1,1 +1,14 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import '../Styles/SearchBar.css'
+
+const search = () => {
+    return "SearchBar Here Please"
+    /*let poke = useSelector(poke => poke.pokemon);
+    let dispatch = useDispatch();
+
+    useEffect( () => () => { return dispatch()}, [] )*/
+}
+
+export default search
