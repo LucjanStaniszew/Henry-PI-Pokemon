7700f17766d26cb5bcaad88b3edd102263689381
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-02-10 21:04:06 -0300

    Added all rest of action types (getTypes, getOnePoke & filters)

--- client/src/Actions/index.js
+++ client/src/Actions/index.js
@@ -10,6 +10,16 @@ export function getPokes(){
     }
 }
 
+export function getTypes(){
+    return async (dispatch) => {
+        var types = await axios.get('http://localhost:3001/types')
+        return dispatch({
+            type: "GET_TYPES",
+            payload: types.data
+        })
+    }
+}
+
 export function getOnePoke(name) {
     return async (dispatch) => {
         var poke = await axios.get('http://localhost:3001/pokemons?name=' + name)
@@ -17,5 +27,36 @@ export function getOnePoke(name) {
             type: "GET_ONE_POKE",
             payload: poke.data
         })
+    }
+}
+
+                        // Filters
+
+export function filterByAbc (payload) {
+    return {
+        type: "FILTER_BY_ABC",
+        payload
+    }
+}
+
+export function filterByType (payload) {
+    console.log(payload)
+    return {
+        type: "FILTER_BY_TYPE",
+        payload
+    }
+}
+
+export function filterByStrength (payload) {
+    return {
+        type: "FILTER_BY_STRENGTH",
+        payload
+    }
+}
+
+export function filterByApi (payload) {
+    return {
+        type: "FILTER_BY_API",
+        payload
     }
 }
