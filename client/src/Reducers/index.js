8e43902591db25d000cd6449dc70f751219acf53
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-02-10 21:05:08 -0300

    Added others reducers ( GET_ONE_POKE, GET_TYPES & Filters)

--- client/src/Reducers/index.js
+++ client/src/Reducers/index.js
@@ -9,17 +9,53 @@ const initialState = {
 
 function rootReducer(state = initialState, action) {
     switch(action.type) {
+
         case 'GET_POKEMONS':
             return {
                 ...state,
-                pokemons: action.payload
+                pokemons: action.payload,
+                pokemonsFiltered: action.payload
             }
 
-            case 'GET_ONE_POKE':
+        case 'GET_ONE_POKE':
+            return {
+                ...state,
+                pokemon: action.payload
+            }
+
+        case 'GET_TYPES':
+            return {
+                ...state,
+                pokemonsTypes: action.payload
+            }
+
+        /*case 'FILTER_BY_ABC':
+            if( action.payload === "all" ){
                 return {
                     ...state,
-                    pokemon: action.payload
+                    pokemons: state.pokemons
+                } else {
+                    let orderPokes = action.payload
                 }
+            }
+            const allPokes = state.pokemons;
+            const pokeFilter = action.payload === "all" ? allPokes : allPokes.filter
+            return {
+                ...state,
+
+            }*/
+            
+        case 'FILTER_BY_TYPE':
+            const allPokes = state.pokemonsFiltered;
+            const typeFiltered = action.payload === "all" ? allPokes :
+                allPokes.filter( p => p.types.map( pt => pt.name ).includes( action.payload ))
+            return{
+                ...state,
+                pokemons: typeFiltered
+
+            }
+        
+        
 
      default:
         return state
