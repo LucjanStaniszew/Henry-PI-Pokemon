7490b7aad5468663f71d01c69fc3977530059d99
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-02-13 13:12:50 -0300

    SearchBar hardly updated with search function, images, inputs and styles

--- client/src/Components/React/SearchBar.jsx
+++ client/src/Components/React/SearchBar.jsx
@@ -1,14 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-
+import { getPoke } from '../../Actions'
 import '../Styles/SearchBar.css'
 
-const search = () => {
-    return "SearchBar Here Please"
-    /*let poke = useSelector(poke => poke.pokemon);
-    let dispatch = useDispatch();
+export default function Search() {
+    const dispatch = useDispatch();
+    const [ name, setName ] = useState("")
 
-    useEffect( () => () => { return dispatch()}, [] )*/
+    const handleSearchBar = (e) => {
+        setName(e.target.value)
+        console.log(name)
+    }
+
+    const handleSubmit = (e) => {
+        //e.prevent.default()
+        dispatch(getPoke(name))
+    }
+
+    return (
+        <div>
+            <input className='searchInput' type='text' placeholder='Search Pokemons...' onChange={(e) => handleSearchBar(e)} />
+            <button className='pokeSearch' type='submit' onClick={(e) => handleSubmit(e)} />
+        </div>
+    )
 }
-
-export default search
