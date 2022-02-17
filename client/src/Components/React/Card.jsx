4118a9f1f49df04ffd90913cf396a24175beb7fe
Luciano Staniszewski <luciano.stani97@gmail.com>
2022-02-17 11:04:45 -0300

    Card updated

--- client/src/Components/React/Card.jsx
+++ client/src/Components/React/Card.jsx
@@ -5,12 +5,12 @@ import '../Styles/Card.css'
 export default function Card ({ id, img, name, types}) {
     return (
         <div className='Card'>
-                <h4 className='pokemon'>Pokemon: {name}</h4>
-                <Link to={"/detail/" + id}>
-                    <img className='pokeImg' src={img} alt="PokeImage not found" width="175px" height="160px" />
-                </Link>
-                <h5 className='types'>Types: {types.map(pt => "- " + pt.name + " ")}</h5>
-                <br />
+            <h4 className='pokemon'>Pokemon: {name}</h4>
+            <Link to={"/pokemons/" + id}>
+                <img className='pokeImg' src={img} alt="PokeImage not found" width="175px" height="160px" />
+            </Link>
+            <h5 className='types'>Types: {types.map(pt => "- " + pt.name + " ")}</h5>
+            <br />
         </div>
     )
 }
